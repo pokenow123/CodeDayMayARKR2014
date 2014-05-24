61ba3a4120e4244c6dc2e0d9952cb8ef701a8504
unknown <s-mberger@487M009546.lwsd.k12wa.org>
2014-05-24 15:47:30 -0700

    Classes modified
    Signed-off-by: unknown <s-mberger@487M009546.lwsd.k12wa.org>

--- TheAdventuresOfCaptainBlad/TheAdventuresOfCaptainBlad/TheAdventuresOfCaptainBlad/Ship.cs
+++ TheAdventuresOfCaptainBlad/TheAdventuresOfCaptainBlad/TheAdventuresOfCaptainBlad/Ship.cs
@@ -7,5 +7,25 @@ namespace TheAdventuresOfCaptainBlad
 {
     class Ship
     {
+        public Ship()
+        {
+
+        }
+
+        public int HULL;
+        public int SPEED;
+        public int CREW;
+
+        public Tile[,] SHIPGRID;
+
+        //screw gitHub to the sticking place
+        //hull - regenerates over time
+        //crew  - doesn't regenerate	
+        //once hull drops below 75%, crew start dying. If either one is 0, you die
+        //speed - 
+            //impacts how hard it is to hit your vessel (some)
+            //*impacts how far you can go on the world map
+        //maneuverbility - impacts how hard it is to hit your vessel (a lot)
+        //
     }
 }
